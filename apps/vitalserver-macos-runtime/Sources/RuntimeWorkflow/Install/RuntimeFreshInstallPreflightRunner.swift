f099876d2e3c2a98dbaf60fa4b6d2471bad86386
Seungbae Ji <seungbae.ji@gmail.com>
2026-06-05 06:33:40 +0900

    refactor: move fresh install preflight workflow
    Refs #46

---
+++ apps/vitalserver-macos-runtime/Sources/RuntimeWorkflow/Install/RuntimeFreshInstallPreflightRunner.swift
@@ -0,0 +1,36 @@
+import Contracts
+import Core
+
+public struct RuntimeFreshInstallPreflightRunner {
+    public var settingsState: () -> RuntimeInstallSettingsState
+    public var artifactStates: () -> [RuntimeInstallArtifactState]
+    public var serviceStates: () -> [RuntimeFreshInstallServiceState]
+    public var packageReceiptStates: () -> [RuntimePackageReceiptState]
+    public var proxyPortState: (Int) -> RuntimeHostProxyPortState
+
+    public init(
+        settingsState: @escaping () -> RuntimeInstallSettingsState,
+        artifactStates: @escaping () -> [RuntimeInstallArtifactState],
+        serviceStates: @escaping () -> [RuntimeFreshInstallServiceState],
+        packageReceiptStates: @escaping () -> [RuntimePackageReceiptState],
+        proxyPortState: @escaping (Int) -> RuntimeHostProxyPortState
+    ) {
+        self.settingsState = settingsState
+        self.artifactStates = artifactStates
+        self.serviceStates = serviceStates
+        self.packageReceiptStates = packageReceiptStates
+        self.proxyPortState = proxyPortState
+    }
+
+    public func run() -> RuntimeFreshInstallPreflightDocument {
+        let settings = settingsState()
+        let proxyState = settings.proxyPort.map(proxyPortState)
+        return RuntimeFreshInstallPreflightPolicy.document(input: RuntimeFreshInstallPreflightInput(
+            settingsState: settings,
+            artifactStates: artifactStates(),
+            serviceStates: serviceStates(),
+            packageReceiptStates: packageReceiptStates(),
+            proxyPortState: proxyState
+        ))
+    }
+}
