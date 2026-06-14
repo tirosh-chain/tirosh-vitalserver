a421ac4d51890041e1e5efcf32870b39238e5184
Seungbae Ji <seungbae.ji@gmail.com>
2026-06-14 17:19:32 +0900

    fix: update helper and launcher versions to 0.1.15

--- apps/vitalserver-macos-runtime/Sources/Bootstrap/Composition/GeneratedVersion.swift
+++ apps/vitalserver-macos-runtime/Sources/Bootstrap/Composition/GeneratedVersion.swift
@@ -4,7 +4,7 @@
 import Contracts
 
 public extension Constants {
-    static let launcherVersion = "0.1.14"
+    static let launcherVersion = "0.1.15"
     static let launcherChannel = UpdateBundleChannel(
         rawValue: "dev"
     )
